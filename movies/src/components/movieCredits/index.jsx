1529589d7c06d8a53640c66ded060f7d92051087
ice <20107850@mail.wit.ie>
2026-03-24 15:51:16 +0000

    Tagging upcoming movies as must-watch - completed Movie Labs!

--- movies/src/components/movieCredits/index.jsx
+++ movies/src/components/movieCredits/index.jsx
@@ -2,6 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { getMovieCredits } from "../../api/tmdb-api";
 import Spinner from "../spinner";
 
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
 const MovieCredits = ({ movie }) => {
   const { data, error, isPending, isError } = useQuery({
     queryKey: ["credits", { id: movie.id }],
